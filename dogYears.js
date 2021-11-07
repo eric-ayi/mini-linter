942713fe978bb3c416e72e7be510edb08dea84ce
ericy1 <eric.ayi@amalitech.org>
2021-11-07 13:34:59 +0000

    first commit

---
+++ dogYears.js
@@ -0,0 +1,20 @@
+// creating a variable and setting it to a human age.
+const myAge = 30;
+// creating a variable for the first two years.
+var earlyYears = 2;
+// converting the first two years into dog years
+earlyYears *= 10.5;
+// subtracting the first two years from the human age and setting it to laterYears
+var laterYears = myAge - 2;
+laterYears *= 4;
+//console.log(earlyYears);
+//console.log(laterYears);
+// sum of age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+// using a string method to convert the name to lower case letters.
+var myName = 'ERIC'.toLowerCase();
+// displaying on a console.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is  ${myAgeInDogYears} years old in dog years.`)
+
+
+
